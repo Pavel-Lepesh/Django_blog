6e5876e05afe0da735c846708efc21210d5ab6c5
Pavel Lepeshinsky <pashal200015@gmail.com>
2023-07-13 11:27:32 +0300

    2

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,12 +6,18 @@ from .models import Profile
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
-    username = forms.CharField(max_length=30)
-    email = forms.EmailField(max_length=200)
-    password1 = forms.CharField(max_length=50, widget=forms.PasswordInput())
-    password2 = forms.CharField(max_length=50, widget=forms.PasswordInput())
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={"class": "form-control mb-1", 'placeholder': 'Enter First Name'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={"class": "form-control mb-1", 'placeholder': 'Enter Last Name'}))
+    username = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={"class": "form-control mb-1", 'placeholder': 'Enter Username'}))
+    email = forms.EmailField(
+        widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'Enter your E-Mail'}))
+    password1 = forms.CharField(max_length=50, widget=forms.PasswordInput(
+        attrs={"class": "form-control mb-1", 'placeholder': 'Enter password'}))
+    password2 = forms.CharField(max_length=50, widget=forms.PasswordInput(
+        attrs={"class": "form-control mb-1", 'placeholder': 'Confirm Password'}))
 
     class Meta:
         model = User
@@ -21,12 +27,11 @@ class SignUpForm(UserCreationForm):
 class LoginForm(AuthenticationForm):
     username = forms.CharField(max_length=100,
                                required=True,
-                               widget=forms.TextInput(attrs={'placeholder': 'Username'}))
-
+                               widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'Username'}))
     password = forms.CharField(max_length=50,
                                required=True,
-                               widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
-
+                               widget=forms.PasswordInput(
+                                   attrs={"class": "form-control mb-1", 'placeholder': 'Password'}))
     remember_me = forms.BooleanField(required=False)
 
     class Meta:
@@ -37,10 +42,9 @@ class LoginForm(AuthenticationForm):
 class UpdateUserForm(forms.ModelForm):
     username = forms.CharField(max_length=100,
                                required=True,
-                               widget=forms.TextInput())
-
+                               widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'Username'}))
     email = forms.EmailField(required=True,
-                             widget=forms.TextInput())
+                             widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'Username'}))
 
     class Meta:
         model = User
@@ -48,8 +52,9 @@ class UpdateUserForm(forms.ModelForm):
 
 
 class UpdateProfileForm(forms.ModelForm):
-    avatar = forms.ImageField(widget=forms.FileInput())
-    bio = forms.CharField(widget=SummernoteWidget())
+    avatar = forms.ImageField(widget=forms.FileInput(attrs={"class": "form-control mb-1", 'placeholder': 'Username'}))
+    bio = forms.CharField(widget=SummernoteWidget(attrs={"class": "form-control", 'placeholder': 'Enter search term...',
+                                                         'summernote': {'width': '100%', 'height': '300px'}}))
 
     class Meta:
         model = Profile
--- blog/forms.py
+++ blog/forms.py
@@ -4,14 +4,25 @@ from django_summernote.widgets import SummernoteWidget
 
 
 class EmailPostField(forms.Form):
-    name = forms.CharField(max_length=25)
-    email = forms.EmailField()
-    to = forms.EmailField()
-    comments = forms.CharField(required=False, widget=forms.Textarea)
+    name = forms.CharField(max_length=25, required=True,
+                           widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'Name'}))
+    email = forms.EmailField(required=True,
+                             widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'E-Mail'}))
+    to = forms.EmailField(required=True,
+                          widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'To'}))
+    comments = forms.CharField(required=False,
+                               widget=forms.Textarea(attrs={"class": "form-control mb-1", 'placeholder': 'Comments'}))
 
 
 class CommentForm(forms.ModelForm):
-    body = forms.CharField(required=True, widget=SummernoteWidget())
+    name = forms.CharField(required=True,
+                           widget=forms.TextInput(attrs={"class": "form-control", 'placeholder': 'Name'}))
+    email = forms.EmailField(required=True,
+                             widget=forms.EmailInput(attrs={"class": "form-control", 'placeholder': 'Email'}))
+    body = forms.CharField(required=True,
+                           widget=SummernoteWidget(
+                               attrs={"class": "form-control", 'placeholder': 'Enter search term...',
+                                      'summernote': {'width': '100%', 'height': '300px'}}))
 
     class Meta:
         model = Comment
@@ -19,4 +30,5 @@ class CommentForm(forms.ModelForm):
 
 
 class SearchForm(forms.Form):
-    query = forms.CharField()
+    query = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'Enter search term...'}))
